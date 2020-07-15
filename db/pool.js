afd209153b4d6dd4b6a5cbb4f559711a3a8bc79d
DESKTOP-Q0AOHVD\ADMIN <nguyentandat.email07@gmail.com>
2020-07-15 09:33:28 +0700

    no message

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,8 @@
 import { Pool } from 'pg';
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 
-dotenv.config();
-const databaseConfig = { connectionString: process.env.DATABASE_URL };
+// dotenv.config();
+const databaseConfig = { connectionString: 'postgresql://postgres:123@localhost:5432/transportApi' };
 const pool = new Pool(databaseConfig);
 
 export default pool;
